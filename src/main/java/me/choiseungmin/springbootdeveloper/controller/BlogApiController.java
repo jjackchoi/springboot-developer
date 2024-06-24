82c8928ab19da462ebfd325b0bc7c66f4af0f3be
jjackchoi <jackchoi0325@gmail.com>
2024-06-24 16:13:13 +0900

    article 전체 조회, 단일 조회, 삭제, 수정 구현

--- src/main/java/me/choiseungmin/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/choiseungmin/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,14 @@ package me.choiseungmin.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.choiseungmin.springbootdeveloper.domain.Article;
 import me.choiseungmin.springbootdeveloper.dto.AddArticleRequest;
+import me.choiseungmin.springbootdeveloper.dto.ArticleResponse;
+import me.choiseungmin.springbootdeveloper.dto.UpdateArticleRequest;
 import me.choiseungmin.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +18,7 @@ public class BlogApiController {
 
     private final BlogService blogService;
 
+    // 추가
     // HTTP 메서드가 POST일 때 전달받은 URL과 동일하면 메서드로 매핑
     @PostMapping("/api/articles")
     // 요청 본문 값 매핑
@@ -24,5 +27,41 @@ public class BlogApiController {
 
         // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+
+    // 전체 조회
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()                           // List<Article>을 스트림으로 변환
+                .map(ArticleResponse::new)          // 각 Article을 ArticleResponse로 변환
+                .toList();                          // 스트림을 List<ArticleResponse>로 변환
+
+        return ResponseEntity.ok().body(articles);  // 변환된 리스트를 응답으로 반환
+    }
+
+    // 단일 조회
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    // 삭제
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable long id) {
+        blogService.delete(id);
+
+        return ResponseEntity.ok().build();
+    }
+
+    // 수정
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id, @RequestBody UpdateArticleRequest request) {
+        Article updatedArticle = blogService.update(id, request);
+
+        return ResponseEntity.ok().body(updatedArticle);
     }
 }
--- src/main/java/me/choiseungmin/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/choiseungmin/springbootdeveloper/service/BlogService.java
@@ -3,8 +3,12 @@ package me.choiseungmin.springbootdeveloper.service;
 import lombok.RequiredArgsConstructor;
 import me.choiseungmin.springbootdeveloper.domain.Article;
 import me.choiseungmin.springbootdeveloper.dto.AddArticleRequest;
+import me.choiseungmin.springbootdeveloper.dto.UpdateArticleRequest;
 import me.choiseungmin.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +19,32 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // 글 전체 조회
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    // 글 단일 조회
+    public Article findById(long id) {
+        return blogRepository.findById(id) // 없으면 IllegalArgumentException 발생
+                .orElseThrow(() -> new IllegalArgumentException("not found: "+id));
+    }
+
+    // 글 삭제
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
+
+    // 글 수정
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: "+id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
