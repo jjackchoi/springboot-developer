fa964e6140bb1e651888cb42879a39e56f421999
jjackchoi <jackchoi0325@gmail.com>
2024-06-26 15:34:57 +0900

    스프링부트 시큐리티로 인증,인가 적용

---
+++ src/main/java/me/choiseungmin/springbootdeveloper/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package me.choiseungmin.springbootdeveloper.repository;
+
+import me.choiseungmin.springbootdeveloper.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email); // email로 사용자 정보를 가져옴
+}
